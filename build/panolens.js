0bf5e85126ef290d9c9a1c1ac4f291aae4cb966b
Ray Chen <ray.chen.705007@gmail.com>
2016-06-29 10:45:37 -0700

    Update documentation and build

--- build/panolens.js
+++ build/panolens.js
@@ -1707,7 +1707,7 @@ THREE.OrbitControls = function ( object, domElement, passiveUpdate ) {
 
 	}
 
-	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
+	//this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
 	this.domElement.addEventListener( 'mousedown', onMouseDown, false );
 	this.domElement.addEventListener( 'mousewheel', onMouseWheel, false );
 	this.domElement.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
@@ -2610,7 +2610,70 @@ GSVPANO.PanoLoader = function (parameters) {
 
 	};
 
-})();;( function () {
+})();;/**
+ * Stereographic projection shader
+ * based on http://notlion.github.io/streetview-stereographic
+ * @author pchen66
+ */
+
+PANOLENS.StereographicShader = {
+
+	uniforms: {
+
+		"tDiffuse":   { value: new THREE.Texture() },
+        "resolution": { value: 1.0 },
+        "transform":  { value: new THREE.Matrix4() },
+        "zoom": 	  { value: 1.0 }
+
+	},
+
+	vertexShader: [
+
+		"varying vec2 vUv;",
+
+        "void main() {",
+
+        	"vUv = uv;",
+        	"gl_Position = vec4( position, 1.0 );",
+
+        "}" 
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"uniform sampler2D tDiffuse;",
+        "uniform float resolution;",
+        "uniform mat4 transform;",
+        "uniform float zoom;",
+
+        "varying vec2 vUv;",
+
+        "const float PI = 3.141592653589793;",
+
+        "void main(){",
+
+        	"vec2 position = -1.0 +  2.0 * vUv;",
+
+        	"position *= vec2( zoom * resolution, zoom * 0.5 );",
+
+        	"float x2y2 = position.x * position.x + position.y * position.y;",
+        	"vec3 sphere_pnt = vec3( 2. * position, x2y2 - 1. ) / ( x2y2 + 1. );",
+
+        	"sphere_pnt = vec3( transform * vec4( sphere_pnt, 1.0 ) );",
+
+        	"vec2 sampleUV = vec2(",
+        		"(atan(sphere_pnt.y, sphere_pnt.x) / PI + 1.0) * 0.5,",
+        		"(asin(sphere_pnt.z) / PI + 0.5)",
+        	");",
+
+        	"gl_FragColor = texture2D( tDiffuse, sampleUV );",
+
+        "}"
+
+	].join( "\n" )
+
+};;( function () {
 
 	'use strict';
 
@@ -3841,6 +3904,7 @@ GSVPANO.PanoLoader = function (parameters) {
         this.container.addEventListener( 'touchend', this.onMouseUp.bind( this ), false );
         this.container.addEventListener( 'mousewheel', this.onMouseWheel.bind( this ), false );
         this.container.addEventListener( 'DOMMouseScroll', this.onMouseWheel.bind( this ), false );
+        this.container.addEventListener( 'contextmenu', this.onContextMenu.bind( this ), false );
 		
 	};
 
@@ -3854,6 +3918,7 @@ GSVPANO.PanoLoader = function (parameters) {
         this.container.removeEventListener( 'touchend', this.onMouseUp.bind( this ), false );
         this.container.removeEventListener( 'mousewheel', this.onMouseWheel.bind( this ), false );
         this.container.removeEventListener( 'DOMMouseScroll', this.onMouseWheel.bind( this ), false );
+        this.container.removeEventListener( 'contextmenu', this.onContextMenu.bind( this ), false );
 		
 	};
 
@@ -4035,6 +4100,12 @@ GSVPANO.PanoLoader = function (parameters) {
 	PANOLENS.LittlePlanet.prototype.onWindowResize = function () {
 
 		this.material.uniforms.resolution.value = this.container.clientWidth / this.container.clientHeight;
+
+	};
+
+	PANOLENS.LittlePlanet.prototype.onContextMenu = function () {
+
+		this.dragging = false;
 
 	};
 
