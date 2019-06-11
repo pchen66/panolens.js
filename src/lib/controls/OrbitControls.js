c0dffae1c6e6e2c55b4d7b842450173d336d7ccf
Ray Chen <ray@youspace.com>
2019-06-11 14:28:52 -0700

    Add dispose method

--- src/lib/controls/OrbitControls.js
+++ src/lib/controls/OrbitControls.js
@@ -822,6 +822,21 @@ function OrbitControls ( object, domElement ) {
 
     }
 
+    this.dispose = function() {
+
+        this.domElement.removeEventListener( 'mousedown', onMouseDown );
+        this.domElement.removeEventListener( 'mousewheel', onMouseWheel );
+        this.domElement.removeEventListener( 'DOMMouseScroll', onMouseWheel );
+
+        this.domElement.removeEventListener( 'touchstart', touchstart );
+        this.domElement.removeEventListener( 'touchend', touchend );
+        this.domElement.removeEventListener( 'touchmove', touchmove );
+
+        window.removeEventListener( 'keyup', onKeyUp );
+        window.removeEventListener( 'keydown', onKeyDown );
+
+    };
+
     // this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
     this.domElement.addEventListener( 'mousedown', onMouseDown, { passive: false } );
     this.domElement.addEventListener( 'mousewheel', onMouseWheel, { passive: false } );
